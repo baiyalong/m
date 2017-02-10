01441546a6b3f67d5ad1fa10576b618b17e074ea
baiyalong <baiyalong@gmail.com>
2017-02-10 16:51:05 +0800

    xterm

--- imports/ui/component/console.jsx
+++ imports/ui/component/console.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
+import Terminal from 'xterm'
 
 const customContentStyle = {
     width: '100%',
@@ -16,7 +17,16 @@ export default class Console extends React.Component {
         super()
         this.state = {};
     }
-
+    connect() {
+        var term = new Terminal({
+            cursorBlink: true, // Do not blink the terminal's cursor
+            cols: 120, // Set the terminal's width to 120 columns
+            rows: 80 // Set the terminal's height to 80 rows
+        });
+        term.open(document.getElementById('terminal'));
+        term.write('Hello wrold ! ')
+    }
+    disconnect() {}
     handleClose() {
         this
             .props
@@ -25,27 +35,26 @@ export default class Console extends React.Component {
     };
 
     render() {
-        const actions = [ < FlatButton label = "Cancel" primary = {
-                true
-            }
-            onTouchTap = {
-                () => this.handleClose()
-            } />, < FlatButton label = "Submit" primary = {
+        const actions = [ < FlatButton label = "Connect" primary = {
                 true
             }
             keyboardFocused = {
                 true
             }
             onTouchTap = {
+                () => this.connect()
+            } />, < FlatButton label = "Disconnect" primary = {
+                true
+            }
+            onTouchTap = {
+                () => this.disconnect()
+            } />, < FlatButton label = "Close" primary = {
+                true
+            }
+            onTouchTap = {
                 () => this.handleClose()
             } />
         ];
-
-        const content = (
-            <div>
-                <textarea></textarea>
-            </div>
-        )
 
         return (
             <Dialog
@@ -55,7 +64,7 @@ export default class Console extends React.Component {
                 modal={true}
                 contentStyle={customContentStyle}
                 open={this.props.open}>
-                {content}
+                <div id='terminal'></div>
             </Dialog>
         );
     }
