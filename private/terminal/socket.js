fdea6b79f5f9c5304180ac2fade283dc168ef054
baiyalong <baiyalong@gmail.com>
2017-02-20 09:32:56 +0800

    xterm

--- private/terminal/socket.js
+++
@@ -1,9 +0,0 @@
-module.exports = io => {
-    io
-        .on('connection', function (socket) {
-            socket.emit('news', {hello: 'world'});
-            socket.on('my other event', function (data) {
-                console.log(data);
-            });
-        });
-}
